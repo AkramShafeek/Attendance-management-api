67d64ff97bc69bd6ac2af0a96236ad6cd332d4d5
Akram <akramshafeek70@gmail.com>
2023-08-16 23:43:35 +0530

    Added test files

---
+++ src/app.js
@@ -0,0 +1,39 @@
+// config
+require('express-async-errors');
+require('dotenv').config();
+const cors = require('cors');
+
+// express
+const express = require('express');
+const app = express();
+
+// error handler middleware
+const errorHandlerMiddleware = require('./middlewares/errorHandler');
+
+// routes
+const adminRouter = require('./routes/adminRoutes');
+const facultyRouter = require('./routes/facultyRoutes');
+
+// head middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes config
+app.use('/api/v1/admin/', adminRouter);
+app.use('/api/v1/faculty/',facultyRouter);
+
+// test route
+app.get("/test", (req, res) =>  {
+  res.status(200).send("Hello world");
+})
+
+// error handling for 404
+app.use('*', (req, res) => {
+  throw new Error("Route doesn't exist");
+})
+
+// config error handling
+app.use(errorHandlerMiddleware);
+
+// export app to index.js to start the server
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,8 @@
-// config
-require('express-async-errors');
-require('dotenv').config();
-const cors = require('cors');
-
-// express
-const express = require('express');
-const app = express();
-
-// error handler middleware
-const errorHandlerMiddleware = require('./middlewares/errorHandler');
+// importing app
+const app = require('./app');
 
 // database connection
 const connectToMongo = require('./database/connectToMongo');
-
-// routes
-const adminRouter = require('./routes/adminRoutes');
-const facultyRouter = require('./routes/facultyRoutes');
-
-// head middlewares
-app.use(cors());
-app.use(express.json());
-
-// routes config
-app.use('/api/v1/admin/', adminRouter);
-app.use('/api/v1/faculty/',facultyRouter);
-
-
-app.use('*', (req, res) => {
-  throw new Error("Route doesn't exist");
-})
-app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 4001;
 
